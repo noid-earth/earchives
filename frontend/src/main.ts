1005f0dc232aa3e3b2dab97807685757cb6aaf44
jurgenjacobsen <jurgenjacobsen@outlook.com>
2022-12-05 22:20:11 +0000

    Deleted frontend

--- frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-
-import Header from "./components/Header.vue";
-
-import App from './App.vue';
-
-import './assets/main.css';
-
-createApp(App).component('Header', Header).mount('#app')
