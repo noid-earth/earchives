f77659ed2e563e2081a17aac69a2b83b3e5d9aa3
jurgenjacobsen <jurgenjacobsen@outlook.com>
2022-12-22 16:35:21 +0000

    ...

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -1,5 +1,6 @@
-require('update-electron-app')()
 const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require("electron-updater")
+
 const path = require('path');
 const DiscordRPC = require('discord-rpc');
 
@@ -11,19 +12,20 @@ let mainWindow;
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     center: true,
     autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      devTools: false,
     },
     icon: path.resolve(__dirname, '../build/icon.ico'),
   });
 
-  mainWindow.loadURL('http://localhost');
-
   mainWindow.maximize();
+
+  mainWindow.loadURL('http://localhost');
 
   mainWindow.on('close', (event) => {
     app.quit();
@@ -53,8 +55,37 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+function sendStatusToWindow(text) {
+  win.webContents.send('message', text);
+}
