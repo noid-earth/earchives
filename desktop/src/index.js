75beb5035a7ac1c41c032130d6001cdc670d4cef
jurgenjacobsen <jurgenjacobsen@outlook.com>
2022-12-22 16:44:31 +0000

    Auto updater will be added later

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -1,5 +1,4 @@
 const { app, BrowserWindow } = require('electron');
-const { autoUpdater } = require("electron-updater")
 
 const path = require('path');
 const DiscordRPC = require('discord-rpc');
@@ -59,33 +58,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
-})
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
-});
-
-app.on('ready', function()  {
-  autoUpdater.checkForUpdatesAndNotify();
-});
-
-function sendStatusToWindow(text) {
-  win.webContents.send('message', text);
-}
